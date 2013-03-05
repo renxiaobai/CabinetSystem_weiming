88b613431de1aad18798889b721a17eefe4af971
Ren Xiaobai <renxiaobai@yahoo.com.cn>
2013-03-05 13:31:16 +0800

    robot has empty box

---
+++ CabinetSystem/Robot.cs
@@ -0,0 +1,10 @@
+﻿namespace CabinetSystemTest
+{
+    public class Robot
+    {
+        public bool HasEmptybox()
+        {
+            return true;
+        }
+    }
+}
--- CabinetSystemTest/CabinetTest.cs
+++ CabinetSystemTest/CabinetTest.cs
@@ -74,6 +74,7 @@ namespace CabinetSystemTest
 
             Assert.IsNull(cabinet.Store(bag));
         }
+
     }
 }
 
