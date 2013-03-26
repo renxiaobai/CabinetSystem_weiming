327e10e22efaac337e5c9be7e0fe4995085669b3
Ren Xiaobai <renxiaobai@yahoo.com.cn>
2013-03-26 13:04:21 +0800

    SmartRobot has empty box

---
+++ CabinetSystem/SmartRobot.cs
@@ -0,0 +1,19 @@
+﻿using CabinetSystem;
+
+namespace CabinetSystemTest
+{
+    public class SmartRobot
+    {
+        public void Add(Cabinet cabinet)
+        {
+            
+
+        }
+
+        public bool HasEmptybox()
+        {
+            return true;
+
+        }
+    }
+}
---
+++ CabinetSystemTest/SmartRobotTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using CabinetSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CabinetSystemTest
+{
+    /// <summary>
+    /// Summary description for SmartRobotTest
+    /// </summary>
+    [TestClass]
+    public class SmartRobotTest
+    {
+        [TestMethod]
+        public void should_return_true_given_has_empty_box()
+        {
+            var smartRobot = new SmartRobot();
+            smartRobot.Add(new Cabinet(50));
+            Assert.IsTrue(smartRobot.HasEmptybox());
+        }
+    }
+} 
